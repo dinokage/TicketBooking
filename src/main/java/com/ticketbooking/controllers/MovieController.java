f8b743988a205784e3e10d372a340c9c8c311506
Anisetty <tejesh.anisetty@capgemini.com>
2024-01-29 21:47:18 +0530

    added validation to movie class

--- src/main/java/com/ticketbooking/controllers/MovieController.java
+++ src/main/java/com/ticketbooking/controllers/MovieController.java
@@ -2,13 +2,19 @@ package com.ticketbooking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import com.ticketbooking.dto.MovieDTO;
 import com.ticketbooking.entities.Movie;
 import com.ticketbooking.services.MovieService;
 
+import jakarta.validation.Valid;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -17,8 +23,23 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+//    @PostMapping
+//    public ResponseEntity<?> saveMovie(@RequestBody MovieDTO movieDTO) {
+//        Movie movie = movieService.convertToEntity(movieDTO);
+//        movieService.save(movie);
+//        return ResponseEntity.ok().body("Movie saved successfully");
+//    }
+    
     @PostMapping
-    public ResponseEntity<?> saveMovie(@RequestBody MovieDTO movieDTO) {
+    public ResponseEntity<?> saveMovie(@Valid @RequestBody MovieDTO movieDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+
         Movie movie = movieService.convertToEntity(movieDTO);
         movieService.save(movie);
         return ResponseEntity.ok().body("Movie saved successfully");
--- src/main/java/com/ticketbooking/dto/MovieDTO.java
+++ src/main/java/com/ticketbooking/dto/MovieDTO.java
@@ -1,13 +1,27 @@
 package com.ticketbooking.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class MovieDTO {
 
     private int movieId;
+    
+    @NotBlank(message = "Movie name cannot be blank")
     private String movieName;
+    
+    @NotBlank(message = "Movie genre cannot be blank")
     private String movieGenre;
+    
+    @NotBlank(message = "Movie hours cannot be blank")
     private String movieHours;
+    
+    @NotBlank(message = "Movie Language cannot be blank")
     private String movieLanguage;
+    
+    @NotBlank(message = "Movie year cannot be blank")
     private int year;
+    
+    @NotBlank(message = "Movie Descryption cannot be blank")
     private String movieDescription;
 
     public MovieDTO() {
